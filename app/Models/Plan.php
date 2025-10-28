87db9f30e0a762c65c8f718527a7871971f4de68
Joao-ALD <joaovictorald@gmail.com>
2025-10-28 16:30:56 -0300

    removendo arquivos duplicados por engano

--- app/Models/Plan.php
+++ app/Models/Plan.php
@@ -10,10 +10,29 @@ class Plan extends Model
     use HasFactory;
 
     // Permite preencher todos os campos via create() ou update()
-    protected $guarded = []; 
+    protected $guarded = [];
+
+    protected $casts = [
+        'features' => 'array',
+    ];
 
     public function subscriptions()
     {
         return $this->hasMany(Subscription::class);
+    }
+
+    public function getMaxProductsAttribute()
+    {
+        return $this->features['max_products'] ?? 1;
+    }
+
+    public function getMaxPhotosPerProductAttribute()
+    {
+        return $this->features['max_photos_per_product'] ?? 1;
+    }
+
+    public function getCanHighlightProductsAttribute()
+    {
+        return $this->features['can_highlight_products'] ?? false;
     }
 }
--- resources - Copia/js/app.js
+++
@@ -1,9 +0,0 @@
-import './bootstrap';
-
-import Sortable from 'sortablejs';
-window.Sortable = Sortable; // Torna o Sortable global
-
-import Alpine from 'alpinejs';
-window.Alpine = Alpine;
-
-Alpine.start();
